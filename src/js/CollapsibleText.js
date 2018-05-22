3e9694c5836185c05b4f27298d600012c60f26d8
phillippavlich <pavlicpm@mcmaster.ca>
2018-05-21 22:18:03 -0400

    Completed Projects Page
    Added All media queries to page. All formatting is done. JavaScript completed.

--- src/js/CollapsibleText.js
+++ src/js/CollapsibleText.js
@@ -13,3 +13,72 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+function changeTextSize(x) {
+    if (x.matches) { // If media query matches
+    	var element = document.getElementById("fourthBox");
+    	var element2 = document.getElementById("fifthBox");
+    	if(element2.classList.contains("clear")){
+    		element2.classList.remove("clear");
+    	}
+    	if(!element.classList.contains("clear")){
+    		element.classList.add("clear");
+    	}
+    	
+    } 
+    if (y.matches) { // If media query matches
+    	var element = document.getElementById("fourthBox");
+    	var element2 = document.getElementById("fifthBox");
+    	if(element.classList.contains("clear")){
+    		element.classList.remove("clear");
+    	}
+    	if(!element2.classList.contains("clear")){
+    		element2.classList.add("clear");
+    	}
+    	
+    }
+    if (xx.matches) { // If media query matches
+    	var element = document.getElementsByClassName("box");
+    	
+    	if(element[0].classList.contains("col-xs-6")){
+    		for(var i=0; i<element.length;i++){
+    			element[i].classList.remove("col-xs-6");
+    		}
+    		
+    	}
+    	if(!element[0].classList.contains("col-xs-12")){
+    		for(var i=0; i<element.length;i++){
+    			element[i].classList.add("col-xs-12");
+    		}
+    	}
+    	
+    }
+    if (yy.matches) { // If media query matches
+    	var element = document.getElementsByClassName("box");
+    	if(element[0].classList.contains("col-xs-12")){
+    		for(var i=0; i<element.length;i++){
+    			element[i].classList.remove("col-xs-12");
+    		}
+    	}
+    	if(!element[0].classList.contains("col-xs-6")){
+    		for(var i=0; i<element.length;i++){
+    			element[i].classList.add("col-xs-6");
+    		}
+    	}
+    	
+    } else {
+        
+    }
+}
+
+var x = window.matchMedia("(max-width: 1050px)")
+var y = window.matchMedia("(min-width: 1051px)")
+var xx = window.matchMedia("(max-width: 500px)")
+var yy = window.matchMedia("(min-width: 501px)")
+
+changeTextSize(x) // Call listener function at run time
+changeTextSize(xx) // Call listener function at run time
+
+x.addListener(changeTextSize) // Attach listener function on state changes
+y.addListener(changeTextSize) // Attach listener function on state changes
+xx.addListener(changeTextSize) // Attach listener function on state changes
+yy.addListener(changeTextSize) // Attach listener function on state changes
